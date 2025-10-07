95241664b9901b8a3867f0ef11d8242dec13c012
shotxd012 <shotxd506@gmail.com>
2025-10-07 18:30:19 +0000

    remove api

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 // Import all route modules
 const profileRoutes = require("./profile");
-const topUsersRoutes = require("./top-users");
 const mediaRoutes = require("./media");
 const ticketsRoutes = require("./tickets");
 const commentsRoutes = require("./comments");
@@ -11,7 +10,6 @@ const statusRoutes = require("./status");
 
 // Mount routes
 router.use("/profile", profileRoutes);
-router.use("/top-users", topUsersRoutes);
 router.use("/media", mediaRoutes);
 router.use("/tickets", ticketsRoutes);
 router.use("/comments", commentsRoutes);
